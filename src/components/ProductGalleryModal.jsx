c496761855b0638d6bb5321d2abd577d4c083965
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-04-06 22:28:34 -0300

    Update ProductGalleryModal.jsx

--- src/components/ProductGalleryModal.jsx
+++ src/components/ProductGalleryModal.jsx
@@ -2,8 +2,7 @@ import React, { useRef } from 'react';
 import './ProductGallery.css';
 
 const ProductGalleryModal = ({ product, current }) => {
-  const {current} = current;
-  console.log('current', current.src);
+  console.log('current', current.current.src);
   let galleryModal = product.media.images[0].url;
   let { images } = product.media;
   const modalPoster = useRef(null);
@@ -14,7 +13,7 @@ const ProductGalleryModal = ({ product, current }) => {
     <article className="gallery">
       <div className="gallery__image-container">
         <img
-          src={current.src}
+          src={current.current.src}
           //src={`https://${galleryModal}`}
           className="gallery__image"
           ref={modalPoster}
