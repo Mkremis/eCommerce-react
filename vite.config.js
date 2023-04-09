8904fe81589ca4b6585b053ec418b24a644fd81d
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-04-09 17:25:59 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/ecommerce-react-vite/',
+  base:'/ecommerce-react/',
   plugins: [react()],
 })
