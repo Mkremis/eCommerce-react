2cf76dde79f1f376bf8785f21b2e37c91980ab6c
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-04-24 15:07:59 +0000

    styles changes

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -6,8 +6,8 @@ const ProductCard = ({ image, name, price_curr, id }) => {
     <figure id={id}>
       <img className="product-image" src={image} alt={name} />
       <figcaption className="product-description" id={id}>
-        <p className="product-name">{name}</p>
-        <p className="product-price">{price_curr}</p>
+        <span className="product-name">{name}</span>
+        <span className="product-price">{price_curr}</span>
       </figcaption>
     </figure>
   );
