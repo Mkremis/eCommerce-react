4270abd1f6b960a632d7c934ac71790a925345ae
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-04-24 11:25:15 -0300

    7 files modified

--- src/components/Aside.jsx
+++ src/components/Aside.jsx
@@ -1,22 +1,22 @@
-import React, { useContext, useEffect, useState } from "react";
-import { options } from "../helpers/api";
-import { helpHttp } from "../helpers/helpHttp";
-import { useLocation } from "react-router-dom";
-import AuthContext from "../context/AuthContext";
+import React, { useContext, useEffect, useState } from 'react';
+import { options } from '../helpers/api';
+import { helpHttp } from '../helpers/helpHttp';
+import { useLocation } from 'react-router-dom';
+import AuthContext from '../context/AuthContext';
 
-import "./Aside.css";
+import './Aside.css';
 const Aside = () => {
   const { pathname } = useLocation();
   const [response, setResponse] = useState(null);
   const [categories, setCategories] = useState(null);
-  let root = pathname.split("/")[1];
+  let root = pathname.split('/')[1];
   const { refreshPage } = useContext(AuthContext);
 
   // let categories = pathname.includes("sortBy") ? data : null;
   useEffect(() => {
     helpHttp()
       .get(
-        "https://asos2.p.rapidapi.com/categories/list?country=US&lang=en-US",
+        'https://asos2.p.rapidapi.com/categories/list?country=US&lang=en-US',
         options
       )
       .then((res) => setResponse(res));
@@ -24,7 +24,7 @@ const Aside = () => {
 
   useEffect(() => {
     let dataPath;
-    if (response && pathname.includes("sortBy")) {
+    if (response && pathname.includes('sortBy')) {
       dataPath = {
         men: {
           url: response.navigation[0].children[4].children[3].children[1]
@@ -35,7 +35,9 @@ const Aside = () => {
             .children,
         },
       };
-      dataPath[root] ? setCategories(dataPath[root].url) : setCategories(null);
+      setCategories(dataPath[root].url);
+    } else {
+      setCategories(null);
     }
   }, [pathname, root, response]);
 
@@ -50,7 +52,7 @@ const Aside = () => {
                 return (
                   <li key={`${cat.content.title}_${cat.link.categoryId}`}>
                     <button
-                      className={"category__link"}
+                      className={'category__link'}
                       onClick={() =>
                         refreshPage(
                           `/categoryId=${cat.link.categoryId}/sortBy/%20/filter/%20/search/%20/offset/48`
@@ -58,7 +60,7 @@ const Aside = () => {
                       }
                       id={cat.link.categoryId}
                     >
-                      {cat.content.title.replace("SALE", "")}
+                      {cat.content.title.replace('SALE', '')}
                     </button>
                   </li>
                 );
