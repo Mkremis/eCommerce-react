1182621edd7739bbcc91998462b8ce29b4ca6450
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-06-07 13:23:11 +0000

    api

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,6 +1,6 @@
 export const options = {
 	headers: {
-		'X-RapidAPI-Key': '49d9c22181msh41372344189763fp1c86dajsn7ad9035f998d',
+		'X-RapidAPI-Key': 'd210c99c19mshfe716f01deb4167p14f4c9jsn9e4d8f8900ce',
 		'X-RapidAPI-Host': 'asos2.p.rapidapi.com'
 	},
 };
