3e4abf5a074d6c1a186e396572ea0a4b85e49977
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-10 19:35:03 +0000

    Update 4 files

--- src/hooks/useRenderForm.jsx
+++ src/hooks/useRenderForm.jsx
@@ -1,40 +1,91 @@
 import React from "react";
 
-
 const useRenderForm = () => {
-
-  const isType=(key)=>{
+  const isType = (key) => {
     switch (key) {
-      case 'password':
-        return 'password'
-      case 'email':
-        return 'email'
-      case 'phone':
-          return 'tel'
-      case 'thumbnail':
-          return 'url'
-      default: return 'text'
+      case "password":
+        return "password";
+      case "email":
+        return "email";
+      case "phone":
+        return "tel";
+      case "thumbnail":
+        return "url";
+      default:
+        return "text";
     }
-  }
+  };
 
-  const isRequired=(key)=>{
+  const isRequired = (key) => {
     switch (key) {
-      case 'password':
-        return true
-      case 'username':
-        return true
-      case 'first':
-          return true
-      case 'last':
-          return true
-      case 'email':
-            return true
-      case 'phone':
-            return true
+      case "password":
+        return true;
+      case "username":
+        return true;
+      case "email":
+        return true;
     }
-  }
+  };
 
-  
+  const validate = {
+    password: {
+      pattern: ".{7,60}",
+      title: "7 to 60 characters",
+    },
+    username: {
+      pattern: "^[A-Za-z][A-Za-z0-9_]{8,15}$",
+      title: "8 to 15 characters",
+    },
+    title: {
+      pattern: "[A-Za-z]{1,5}",
+      title: "only letters max 5 characters",
+    },
+    first: {
+      pattern: "[A-Za-z]{1,30}",
+      title: "only letters max 30 characters",
+    },
+    last: {
+      pattern: "[A-Za-z]{1,30}",
+      title: "only letters max 30 characters",
+    },
+    email: {
+      pattern: "[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,50}$",
+      title: "characters@characters.domain",
+    },
+    phone: {
+      pattern: "((+|00)?[1-9]{2}|0)[1-9]( ?[0-9]){10}",
+      title: "only numbers and spaces max 10 characters",
+    },
+    thumbnail: {
+      pattern: "https?://.+",
+      title: "Include http://",
+    },
+    city: {
+      pattern: "[A-Za-z]{1,20}",
+      title: "only letters max 20 characters",
+    },
+    state: {
+      pattern: "[A-Za-z]{1,20}",
+      title: "only letters max 20 characters",
+    },
+    number: {
+      pattern: "^[A-Za-z][A-Za-z0-9_]{1,20}$",
+      title: "numbers and letters max 20 characters",
+    },
+    street: {
+      pattern: "^[A-Za-z][A-Za-z0-9_]{1,20}$",
+      title: "numbers and letters max 20 characters",
+    },
+    country: {
+      pattern: "[A-Za-z]{1,20}",
+      title: "only letters max 20 characters",
+    },
+    postcode: {
+      pattern: "^[A-Za-z][A-Za-z0-9_]{1,10}$",
+      title: "numbers and letters max 10 characters",
+    },
+  };
+
   const renderFormElements = (obj, fieldName) => {
     return Object.entries(obj).map(([key, value]) => {
       if (typeof value === "object") {
@@ -59,7 +110,9 @@ const useRenderForm = () => {
               placeholder={key}
               aria-label={key}
               required={isRequired(key)}
-              autocomplete="off"
+              pattern={validate[key] && validate[key].pattern}
+              title={validate[key] && validate[key].title}
+              autoComplete="false"
             />
           </div>
         </div>
