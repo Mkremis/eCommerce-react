ff4d41b23ababe156ebab23a3b18fb82f189d7de
Martin <105755654+Mkremis@users.noreply.github.com>
2023-06-15 19:39:56 +0000

    Update index.html and UserDashboard.jsx

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -12,33 +12,48 @@ const UserDashboard = () => {
           <h1>{auth ? "Dashboard" : "New User Registration"}</h1>
         </div>
         {auth && (
-          <nav className="dashboard_nav">
-            <ul className="dashboard_items">
-              <li>
-                <NavLink
-                  to={`/dashboard/${user.login.username}`}
-                  className="dashboard_link"
-                >
-                  User Data
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to={`/dashboard/orders/${user.login.username}`}
-                  className="dashboard_link"
-                >
-                  Order History
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to={`/dashboard/likeds/${user.login.username}`}
-                  className="dashboard_link"
-                >
-                  Likes
-                </NavLink>
-              </li>
-            </ul>
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <NavLink
+                      to={`/dashboard/${user.login.username}`}
+                      className="nav-link"
+                    >
+                      User Data
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={`/dashboard/orders/${user.login.username}`}
+                      className="nav-link"
+                    >
+                      Order History
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink
+                      to={`/dashboard/likeds/${user.login.username}`}
+                      className="nav-link"
+                    >
+                      Likes
+                    </NavLink>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </nav>
         )}
       </header>
