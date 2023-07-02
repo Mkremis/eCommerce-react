5da4ee8cc57aaf5a6a84b31bae584e8750f70f14
Mkremis <105755654+Mkremis@users.noreply.github.com>
2023-07-02 01:23:05 -0300

    Update loaderDashboard.js

--- src/helpers/loaderDashboard.js
+++ src/helpers/loaderDashboard.js
@@ -16,7 +16,7 @@ console.log(accessToken)
       },
     };
     const DASH_URL = `https://ecommerce-users-api-production.up.railway.app/api/users/dashboard/${username}`;
-    const response = await axios.post(DASH_URL, options);
+    const response = await axios.post(DASH_URL,{}, options);
 
     if (response.status !== 200) throw new Error(responseUserData.message);
     const { user } = response.data;
