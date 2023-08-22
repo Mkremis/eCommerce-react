3861d939a7766d380aa1b5cab95f4ef6f85f1286
Martin <105755654+Mkremis@users.noreply.github.com>
2023-08-22 14:54:11 +0000

    Update 5 files

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,6 @@
 import axios from "axios";
-// // axios.defaults.withCredentials = true
 
 const client = axios.create({
-  // 'https://ecommerce-users-api-production.up.railway.app'
   baseURL: "https://ecommerce-users-api-production.up.railway.app",
   withCredentials: true,
 });
