2e87e5ff3ce46099e2c3f6b6c950e09274be84f5
Mkremis <martinkremis@hotmail.com>
2024-04-04 20:44:26 -0300

    fix

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -5,7 +5,6 @@ import loaderOrders from "./loaderPurchases";
 import loaderSort from "./loaderSort";
 import loaderLikes from "./loaderLikes";
 import loaderPurchases from "./loaderPurchases";
-import loaderTransaction from "./loaderTransaction";
 
 export {
   loaderDashboard,
@@ -15,5 +14,4 @@ export {
   loaderSort,
   loaderLikes,
   loaderPurchases,
-  loaderTransaction,
 };
