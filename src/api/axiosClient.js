ebf3972252f0e3c694f34ae481d8fec46a1b38a5
Mkremis <martinkremis@hotmail.com>
2024-04-04 21:39:13 -0300

    endopoint

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,7 @@ import axios from "axios";
 //http://localhost:8080
 
 const client = axios.create({
-  baseURL: "https://76d8-190-211-89-1.ngrok-free.app",
+  baseURL: "https://ecommerce-users-api-node-express-dev-hjbp.2.us-1.fl0.io",
   withCredentials: true,
   crossDomain: true,
 });
